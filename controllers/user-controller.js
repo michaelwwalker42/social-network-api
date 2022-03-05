83d34180601f2736c58f428a8a27be44df2b41d0
Michael Walker <michaelwwalker42@gmail.com>
2022-03-05 14:44:06 -0700

    remove user from associated friends on deletion

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,17 +1,23 @@
 const { User, Thought } = require('../models')
 
 const userController = {
-  createUser(req, res) {
-    User.create(req.body)
-      .then(userData => res.json(userData))
-      .catch(err => res.json(err));
-  },
+  // GET all users route 
+  //  /api/users
   getUsers(req, res) {
     User.find()
       .select('-__v')
       .then(userData => res.json(userData))
       .catch(err => res.status(400).json(err));
   },
+  // POST user route  
+  //    /api/users/:userId
+  createUser(req, res) {
+    User.create(req.body)
+      .then(userData => res.json(userData))
+      .catch(err => res.json(err));
+  },
+  // GET user route 
+  //   /api/users/:userId  
   getSingleUser(req, res) {
     User.findOne(
       { _id: req.params.userId }
@@ -38,6 +44,8 @@ const userController = {
       })
       .catch(err => res.status(400).json(err));
   },
+  // PUT user route  
+  //   /api/users/:userId
   updateUser(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
@@ -47,7 +55,8 @@ const userController = {
       .then(userData => res.json(userData))
       .catch(err => res.status(400).json(err));
   },
-
+  //  DELETE user route  
+  //  /api/users/:userId
   deleteUser(req, res) {
     User.findOneAndDelete(
       { _id: req.params.userId },
@@ -57,7 +66,7 @@ const userController = {
           res.status(404).json({ message: 'No user found with this id' })
           return;
         }
-
+        // delete user's thoughts
         Thought.deleteMany({ username: userData.username })
           .then(thoughtData => {
             if (!thoughtData) {
@@ -65,11 +74,24 @@ const userController = {
               return;
             }
           })
+        // remove user from associated friends lists
+        User.updateMany(
+          // The $in operator selects the documents where the value of a field equals any value in the specified array
+          { _id: { $in: userData.friends } },
+          { $pull: { friends: req.params.userId } }
+        )
+          .then(userData => {
+            if (!userData) {
+              res.status(404).json({ message: 'Could not remove user from friends' })
+              return;
+            }
+          })
         res.json({ message: 'User deleted' })
       })
       .catch(err => res.status(400).json(err));
   },
-
+  // POST friend route
+  // /api/users/:userId/friends/:friendId
   addFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
@@ -85,6 +107,8 @@ const userController = {
       })
       .catch(err => res.status(400).json(err));
   },
+  // DELETE friend route
+  // /api/users/:userId/friends/:friendId
   deleteFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
